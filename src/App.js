bd233e319f6d341e130ed6457dc614628d35454c
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-06-13 11:42:57 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { TextField, Button, Paper } from "@material-ui/core";
 
-function App() {
+// Использование ссылок в функциональном компоненте
+export const AppRef = () => {
+  const text = React.useRef();
+  const counter = React.useRef(0);
+  const [ignored, forceUpdate] = React.useReducer((x) => {
+    x = x + 1;
+    return x;
+  }, 0);
+
+  // const [count, setCount] = React.useState(1)
+
+  counter.current++;
+
+  const handleClick = () => {
+    alert(text.current.value + ignored);
+    forceUpdate();
+    // setCount(count + 1);
+    text.current.value = "";
+    console.log(counter);
+  };
+
   return (
-    <div className="App">
-      
-    </div>
+    <Paper elevation={3}>
+      <TextField label="Outlined" variant="outlined" inputRef={text} />
+      <Button variant="contained" color="primary" onClick={handleClick}>
+        Показать текст в алерте
+      </Button>
+      <p>{counter.current}</p>
+    </Paper>
   );
-}
+};
 
-export default App;
+// Использование ссылок в классовом компоненте
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.text = React.createRef();
+    this.counter = React.createRef(1);
+  }
+
+  handleClick = () => {
+    alert(this.text.current.value);
+    this.text.current.value = "";
+    this.forceUpdate();
+    console.log(this.counter);
+  };
+
+  render() {
+    this.counter.current++;
+    return (
+      <Paper elevation={3}>
+        <TextField label="Outlined" variant="outlined" inputRef={this.text} />
+        <Button variant="contained" color="primary" onClick={this.handleClick}>
+          Показать текст в алерте
+        </Button>
+        <p>{this.counter.current}</p>
+      </Paper>
+    );
+  }
+}
